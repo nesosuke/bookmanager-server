ca4b669373f2b695e8462ce1147a51c40f5b82fc
nesosuke <neso@neso.tech>
2022-02-03 00:21:48 +0900

    Revert "fix typo issue #16"

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -33,7 +33,7 @@ def test_register_validate_input(client, username, password, message):
 def test_login(client, auth):
     assert client.get('/auth/login').status_code == 200
     response = auth.login()
-    assert response.headers['Location'] == 'http://localhost/record/'
+    assert response.headers['Location'] == 'http://locahost/record/'
 
     with client:
         client.get('/')

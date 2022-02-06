b3071c14f7a43be1c6550f092c9d39c5f53f85f3
nesosuke <neso@neso.tech>
2022-02-06 14:30:55 +0900

    User.registerの追加

--- server/User.py
+++ server/User.py
@@ -1,4 +1,4 @@
-from flask import check_password_hash
+from flask import check_password_hash, generate_password_hash
 from .Db import get_db as db
 
 
@@ -33,3 +33,22 @@ def validate_user(username, password) -> bool:
         validated = True
 
     return validated
+
+
+def register(username, password) -> bool:
+    '''
+    register a new user
+    table: user
+    search query: username
+    return: boolean
+    '''
+    isexist = findone(username)
+    if isexist:
+        return False
+    else:
+        password = generate_password_hash(password)
+        db.execute(
+            'INSERT INTO user (username, password) VALUES (?, ?)',
+            (username, password))
+        db.commit()
+        return True

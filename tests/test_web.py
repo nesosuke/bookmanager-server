d474b76a8eb16d0c32864d862dde16870e4744a3
nesosuke <neso@neso.tech>
2022-03-27 18:37:22 +0900

    fix(server/test): :bug: pytestのパラメータの括弧のミス

--- tests/test_web.py
+++ tests/test_web.py
@@ -9,7 +9,7 @@ import json
 def test_web_top(client):
     response = client.get('/web/')
     assert response.status_code == 200
-    assert b'<title>' in response.data
+    assert b'this is toppage.' in response.data
 
 # test /web/book/<isbn>
 
@@ -17,23 +17,23 @@ def test_web_top(client):
 @pytest.mark.parametrize(('isbn', 'message'), (
     # book found
     ('9784873119328',
-        {
+        (
             200,
             {
-                b'9784873119328',
-                b'Python 3',
-                b'Lubanovic, Bill',
-                b'オライリー・ジャパン',
+                '9784873119328',
+                'Python 3',
+                'Lubanovic, Bill',
+                'オライリー・ジャパン',
             }
-        }
+        )
      ),
     # book not found
     ('9784873119329',
-        {200,
+        (200,
             {
                 b'Not found'
             }
-         }
+        )
      )))
 def test_web_book_detail(client, isbn, message):
     response = client.get('/web/book/' + isbn)
@@ -45,44 +45,44 @@ def test_web_book_detail(client, isbn, message):
 @pytest.mark.parametrize(('record_id', 'message'), (
     # record found
     ('1',
-        {
+        (
             200,
             {
-                b'9784873119328',
-                b'Python 3',
-                b'Lubanovic, Bill',
-                b'オライリー・ジャパン',
-                b'test',
-                b'read',
-                b'5',
-                b'test comment',
-                b'2022-01-01T00:00:00'
+                '9784873119328',
+                'Python 3',
+                'Lubanovic, Bill',
+                'オライリー・ジャパン',
+                'test',
+                'read',
+                '5',
+                'test comment',
+                '2022-01-01T00:00:00'
             }
-        }
+        )
      ),
     ('2',
-        {
-            200,
+        (
+           200,
             {
                 b'Not found'
             }
-        })))
+        ))))
 def test_web_record_detail(client, record_id, message):
-    response = client.get('/web/record/' + record_id)
+    response=client.get('/web/record/' + record_id)
     assert response.status_code == message[0]
     assert response.data == message[1]
 
 # test /web/record/new
 
 
-@pytest.mark.parametrize(('message'), (
-    {
+@ pytest.mark.parametrize(('message'), (
+    (
         200,
         {
             b'Record create',
             b'submit'}
-    }))
-def test_web_record_new(client,message):
-    response = client.get('/web/record/new')
+)))
+def test_web_record_new(client, message):
+    response=client.get('/web/record/new')
     assert response.status_code == message[0]
     assert response.data == message[1]
